6f7516e85a5f86c8523520eb97d0f35a99e48af7
Yu-Heng Lin <yuhenglin0204@gmail.com>
2023-02-27 01:56:54 -0800

    update

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#include "includes/engine/engine.h"
+using namespace std;
+
+
+// add main entry
+int main()
+{
+    srand(time(NULL));
+    Engine engine;
+    engine.run();
+}
